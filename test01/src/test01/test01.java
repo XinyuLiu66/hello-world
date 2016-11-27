457417d355306c8d9b4baf121241e9214b2b0a14
XinyuLiu66 <449758210@qq.com>
2016-11-27 18:23:01 +0100

    First time to change Hallo-world

---
+++ test01/src/test01/test01.java
@@ -0,0 +1,11 @@
+package test01;
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
